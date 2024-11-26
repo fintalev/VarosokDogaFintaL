9d8195f42a4f72ce40bc89266cec6c1ec8dd2ccd
fintalev <fintalevi@gmail.com>
2024-11-26 10:06:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using VarosokDogaFintaL;
+List<VarosClass> varosok = [];
+
+StreamReader sr = new StreamReader(@"..\..\..\src\varosok.csv");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    varosok.Add(new VarosClass(sr.ReadLine()));
+}
+sr.Close();
+//+
+Console.WriteLine(varosok.Count);
+//1
+Console.WriteLine($"{varosok.Where(v=>v.Orszag=="Kína").Sum(v=>v.Nepesseg)} millió fő él kínai nagyvárosokban");
+//2
+Console.WriteLine($"Indiai városok átlag lélekszáma: {varosok.Where(v=>v.Orszag=="India").Average(v=>v.Nepesseg)}");
+//3
+Console.WriteLine($"A legnépesebb nagyváros: {varosok.OrderByDescending(v=>v.Nepesseg).First()}");
+//4
+var legnepesebbvarosok = varosok.OrderByDescending(v => v.Nepesseg).Where(v => v.Nepesseg > 20);
+Console.WriteLine("20m fő feletti legnépesebb városok:");
+foreach (var item in legnepesebbvarosok)
+{
+    Console.WriteLine(item);
+}
+//5
+Console.WriteLine($"Országok száma amik többször is szerepelnek: {varosok.GroupBy(v=>v.Orszag).Where(v=>v.Count()>1).Count()} db");
+//6
+var kezdobetu = varosok.GroupBy(v => v.Varos[0]).OrderByDescending(group => group.Count()).FirstOrDefault();
+Console.WriteLine($"A legtöbb város neve '{kezdobetu.Key}' betűvel kezdődik, és {kezdobetu.Count()} ilyen város van.");
---
+++ VarosClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarosokDogaFintaL
+{
+    internal class VarosClass
+    {
+        public string Varos { get; set; }
+        public string Orszag { get; set; }
+        public double Nepesseg { get; set; }
+
+        public VarosClass(string s) 
+        {
+            var v=s.Split(';');
+            Varos = v[0];
+            Orszag = v[1];
+            Nepesseg = Math.Round(double.Parse(v[2]),2);
+        }
+        public override string ToString()
+        {
+            return $"{Varos} {Orszag} {Nepesseg}";
+        }
+    }
+}
